17c0cc8c46e676282d9523f7f91d1b0befe4a5c0
Alexandre Rodrigues <alexandrerrodrigues@ua.pt>
2022-05-13 19:06:54 +0100

    projmod3 all figures

--- Mod3/ProjMod3/mod3.m
+++ Mod3/ProjMod3/mod3.m
@@ -14,8 +14,7 @@ format short
 
 global alpha betaA betaP theta1 theta2 theta3 omega gamma epsilon miu miuA miuH sigma zeta niu; 
 
-
-%% Do PDF
+%% Do PDF ParamNorm
 alpha=0.27;
 betaA=0.000878; 
 betaP=0.0000654; 
@@ -32,7 +31,78 @@ theta3=19.7;
 niu=0.000531;
 omega=0.0000000001; %%% onde esta no pdf
 
-% PDF
+%% Parametros simplificados
+% alpha=0.2;
+% betaA=0.0001; 
+% betaP=0.00001; 
+% theta1=0.2;
+% epsilon=2.5;
+% miu=0; 
+% miuA=0;   
+% miuH=0;
+% gamma=0.005;   
+% theta2=0.2; 
+% sigma=0.1;
+% zeta=0.2;
+% theta3=20; 
+% niu=0.0005;
+% omega=0.0000000001;
+
+%% Parametros muito maus ParamMau
+
+% alpha=0.3;
+% betaA=0.01; 
+% betaP=0.001; 
+% theta1=0.5;
+% epsilon=1;
+% miu=0; 
+% miuA=0;   
+% miuH=0;
+% gamma=0.05;   
+% theta2=0.5; 
+% sigma=0.2;
+% zeta=0.01;
+% theta3=20; 
+% niu=0.00001;
+% omega=0.0000000001;
+
+
+%% Parametros muito bons 
+% alpha=0.01;
+% betaA=0; 
+% betaP=0; 
+% theta1=0.01;
+% epsilon=5;
+% miu=0; 
+% miuA=0;   
+% miuH=0;
+% gamma=0.01;   
+% theta2=0.01; 
+% sigma=0.01;
+% zeta=0.01;
+% theta3=1; 
+% niu=0.1;
+% omega=0.0000000001;
+
+%% Parametros bons ParamBom
+% alpha=0.1;
+% betaA=0.0001; 
+% betaP=0.00001; 
+% theta1=0.1;
+% epsilon=3;
+% miu=0; 
+% miuA=0;   
+% miuH=0;
+% gamma=0.01;   
+% theta2=0.1; 
+% sigma=0.05;
+% zeta=0.1;
+% theta3=5; 
+% niu=0.05;
+% omega=0.0000000001;
+
+%% 
+% PDF % usado no VarParam
 % P0=0.095;
 % A0=0.0071;
 % H0=0.000465;
@@ -50,6 +120,10 @@ A0=0.001;
 H0=0.0001;
 R0=0.001;
 
+fig1 = "VariaInicial/Matlab/GeralPDF4.jpg";
+fig2 = "VariaInicial/Matlab/I2PDF4.jpg";
+fig3 = "VariaInicial/Matlab/PDF4.jpg";
+
 %% 
 % - final time -- 
 T = 10; %100
@@ -65,31 +139,74 @@ options = odeset('AbsTol',1e-12,'RelTol',1e-12) ;
 [t, z] = ode45(@sys, [0 T], x0, options);
 
 
-% -------- figures ----------------
-%% 
-figure; 
+%% Making sure S+P+A+H+R=1
+erro1 = mean(sum(z,2) - ones(size(z,1),1));
+fprintf("Erro:%d\n",erro1)  
+
+%% «
+fh = figure();
+fh.WindowState = 'maximized';
 % plot(t, z(:, 1),'-b', t, z(:, 2),'--r', t, z(:, 3), '.-g',t, z(:, 4),'-*y', t, z(:, 5),'-|k', 'LineWidth',1.5);
 plot(t, z(:, 1), t, z(:, 2), t, z(:, 3),t, z(:, 4), t, z(:, 5), 'LineWidth',1.5); 
 xlabel('time');  ylabel('Number individuals'); 
 legend('S', 'P', 'A', 'H', 'R')
+saveas(gcf,fig1)
 
 %% 
-figure; 
+fh = figure();
+fh.WindowState = 'maximized'; 
 % plot(t, z(:, 2),'--r', t, z(:, 3), '.-g',t, z(:, 4),'-*y', t, z(:, 5),'-|k', 'LineWidth',1.5);
 plot(t, z(:, 2), t, z(:, 3),t, z(:, 4), t, z(:, 5), 'LineWidth',1.5);
 xlabel('time');  ylabel('Number individuals'); 
 legend('P', 'A', 'H', 'R')
+saveas(gca,fig2)
 
-%%
-figure; 
-% plot(t, z(:, 3), '.-g',t, z(:, 4),'-*y', t, z(:, 5),'-|k', 'LineWidth',1.5); 
-plot(t, z(:, 3),t, z(:, 4), t, z(:, 5), 'LineWidth',1.5);
-xlabel('time');  ylabel('Number individuals'); 
-legend('A', 'H', 'R')
+% %%
+% figure; 
+% % plot(t, z(:, 3), '.-g',t, z(:, 4),'-*y', t, z(:, 5),'-|k', 'LineWidth',1.5); 
+% plot(t, z(:, 3),t, z(:, 4), t, z(:, 5), 'LineWidth',1.5);
+% xlabel('time');  ylabel('Number individuals'); 
+% legend('A', 'H', 'R')
 
-%% Making sure S+P+A+H+R=1
-erro1 = mean(sum(z,2) - ones(size(z,1),1));
-fprintf("Erro:%d\n",erro1)  
+
+%% Plots
+fh = figure();
+fh.WindowState = 'maximized'; 
+cols = 3;
+rows = 2;
+
+s = z(:, 1); 
+p = z(:, 2); 
+a = z(:, 3); 
+h = z(:, 4);
+r = z(:, 5); 
+
+subplot(rows,cols,1)
+plot(t,s,'LineWidth',1.5); 
+title("Suscetiveis (S)") 
+
+subplot(rows,cols,2)
+plot(t,p,'LineWidth',1.5); 
+title("Prescritos (P)") 
+
+subplot(rows,cols,3)
+plot(t,a,'LineWidth',1.5); 
+title("Viciados em Opioides (A)") 
+
+subplot(rows,cols,4)
+plot(t,h,'LineWidth',1.5); 
+title("Viciados em Heroina (H)") 
+
+subplot(rows,cols,5)
+plot(t,r,'LineWidth',1.5); 
+title("Recuperados (R)") 
+
+% subplot(rows,cols,6)
+% plot(t,u,'LineWidth',1.5); 
+% title("Controlo (u)") 
+
+saveas(gca,fig3)
+
 
 %%
 
--- Mod3/ProjMod3/read_txt.m
+++ Mod3/ProjMod3/read_txt.m
@@ -3,12 +3,23 @@ clear
 close all
 
 %% Read results from file
+name = "PDF4";
+fig1 = sprintf("VariaInicial/Matlab/Geral%s.jpg",name);
+fig2 = sprintf("VariaInicial/Matlab/I2%s.jpg",name);
+fig3 = sprintf("VariaInicial/Matlab/%s.jpg",name);
 
-fileID=fopen('UcontrolVPDF4.txt','r');
+% name = "ParamMau";
+% fig1 = sprintf("VariaParam/Matlab/Geral%s.jpg",name);
+% fig2 = sprintf("VariaParam/Matlab/I2%s.jpg",name);
+% fig3 = sprintf("VariaParam/Matlab/%s.jpg",name);
+
+
+fileID=fopen(sprintf('UcontrolV%s.txt',name),'r');
 formatSpec='%f %f %f %f %f %f %f';
 xsize=[7 Inf];
 x=fscanf(fileID,formatSpec,xsize);
 x=x';
+fclose(fileID);
 
 t = x(:, 1); 
 s = x(:, 2); 
@@ -22,20 +33,59 @@ u = x(:, 7);  % control
 erro1 = mean(sum(x(:,2:6),2) - ones(size(x(:,2:6),1),1));
 fprintf("Erro:%d\n",erro1)
 
-%% Plots
-figure; 
+%% Plots init
+fh = figure();
+fh.WindowState = 'maximized';
 plot(t, s, t, p, t, a, t, h, t, r, 'LineWidth',1.5); 
 legend('s', 'p', 'a', 'h', 'r'); 
-
-figure; 
+saveas(gcf,fig1)
+% 
+fh = figure();
+fh.WindowState = 'maximized'; 
 plot(t, p, t, a, t, h, t, r, 'LineWidth',1.5); 
 legend('p', 'a', 'h', 'r');
+saveas(gcf,fig2)
+% 
+% figure; 
+% plot(t, a, t, h, t, r, 'LineWidth',1.5); 
+% legend('a', 'h', 'r');
+% 
+% figure; 
+% plot(t, u, 'LineWidth',1.5); 
+% legend('u');
 
-figure; 
-plot(t, a, t, h, t, r, 'LineWidth',1.5); 
-legend('a', 'h', 'r');
 
-figure; 
-plot(t, u, 'LineWidth',1.5); 
-legend('u');
+%% Plots
+fh = figure();
+fh.WindowState = 'maximized'; 
+cols = 3;
+rows = 2;
+
+subplot(rows,cols,1)
+plot(t,s,'LineWidth',1.5); 
+title("Suscetiveis (S)") 
+
+subplot(rows,cols,2)
+plot(t,p,'LineWidth',1.5); 
+title("Prescritos (P)") 
+
+subplot(rows,cols,3)
+plot(t,a,'LineWidth',1.5); 
+title("Viciados em Opioides (A)") 
+
+subplot(rows,cols,4)
+plot(t,h,'LineWidth',1.5); 
+title("Viciados em Heroina (H)") 
+
+subplot(rows,cols,5)
+plot(t,r,'LineWidth',1.5); 
+title("Recuperados (R)") 
+
+subplot(rows,cols,6)
+plot(t,u,'LineWidth',1.5); 
+title("Controlo (u)") 
+
+saveas(gcf,fig3)
+
+
 
