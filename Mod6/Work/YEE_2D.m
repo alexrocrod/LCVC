1727513c36e4fd361c39261175e5c2fa1697216e
Alexandre Rodrigues <alexandrerrodrigues@ua.pt>
2022-06-13 17:44:43 +0100

    mod6 code done

--- Mod6/Work/YEE_2D.m
+++ Mod6/Work/YEE_2D.m
@@ -31,7 +31,15 @@ dx=5.0e-3;
 dy=dx;
 
 % time step
-m=0.9; %1.0 1.42 1.41 0.9 1.01
+% m = 1; 
+% Task 4 -> Set m to 1.42 or 1.41
+% m = 1.42;
+% m = 1.41;
+% Task 5 -> Set m to 0.9
+m = 0.9;
+% Task 8 -> Set m to 1.01
+% m = 1.01;
+
 % used time step
 dt=m/sqrt((1/dx)^2+(1/dy)^2)/c;
 
@@ -44,42 +52,54 @@ Hx=zeros(sizex,sizeyp1);
 Hy=zeros(sizexp1,sizey);
 Ez=zeros(sizex,sizey);
 
-
+% Task 6 -> Calculate middle of the x domain
 xmid = floor(sizex/2);
 
+% Task 7 -> Calculate region of y : ymin+35dy<y<ymax-35dy
 ymax = sizey - 35;
 ymin = 35;
 size_excite = ymax - ymin + 1;
 
 % simulation
 for t=1:steps
-
-    % impedence boundary condition
+    
+    % Task 5 -> Implememt the impedence boundary condition
     Ez(1,1:sizey) = Hy(1,1:sizey) * eta0; 
     Ez(sizex,1:sizey) = - Hy(1,1:sizey) * eta0;
-
-   % excitation
-%    Ez(sizex,1:sizey)=ones(1,sizey)*sin(.3*t);
-
-   Hy(xmid,ymin:ymax) = ones(1,size_excite)*sin(.3*t)/eta0; % /eta0 ??
-
-   % update magnetic field
-   Hx(1:sizex,2:sizey)=Hx(1:sizex,2:sizey)+...
-      Db*(Ez(1:sizex,1:sizeym1)-Ez(1:sizex,2:sizey));
-   Hy(2:sizex,1:sizey)=Hy(2:sizex,1:sizey)+...
-      Db*(Ez(2:sizex,1:sizey)-Ez(1:sizexm1,1:sizey));
-
-   % update electric field
-   Ez(1:sizex,1:sizey)=Ez(1:sizex,1:sizey)+...
-      Cb*(Hy(2:sizexp1,1:sizey)-Hy(1:sizex,1:sizey)+...
-      Hx(1:sizex,1:sizey)-Hx(1:sizex,2:sizeyp1));
-
-   if t >= show
-       mesh(Ez);
-       axis([0 sizex 0 sizey -1.5 1.5]); % z de -6 a 6??
-       xlabel('y'); ylabel('x');
     
-       pause(0.1);
-   end
+    % excitation
+    %   Ez(sizex,1:sizey)=ones(1,sizey)*sin(.3*t);
+    
+    % Task 3 -> instead excite Hy at x=xmin=1 
+    %   Hy(1,1:sizey) = ones(1,sizey)*sin(.3*t)/eta0;
+    
+    % Task 6 -> Shift the magentic source location to the middle of x
+    %   Hy(xmid,1:sizey) = ones(1,sizey)*sin(.3*t)/eta0; 
+    
+    % Task 7 -> Restric excitation to a region of domain y
+    Hy(xmid,ymin:ymax) = ones(1,size_excite)*sin(.3*t)/eta0; 
+    
+    % update magnetic field
+    Hx(1:sizex,2:sizey)=Hx(1:sizex,2:sizey)+...
+    Db*(Ez(1:sizex,1:sizeym1)-Ez(1:sizex,2:sizey));
+    Hy(2:sizex,1:sizey)=Hy(2:sizex,1:sizey)+...
+    Db*(Ez(2:sizex,1:sizey)-Ez(1:sizexm1,1:sizey));
+    
+    % update electric field
+    Ez(1:sizex,1:sizey)=Ez(1:sizex,1:sizey)+...
+    Cb*(Hy(2:sizexp1,1:sizey)-Hy(1:sizex,1:sizey)+...
+    Hx(1:sizex,1:sizey)-Hx(1:sizex,2:sizeyp1));
+    
+    % Task 2 -> Plotting starts from time step t = 30 
+    if t >= show
+        mesh(Ez);   
+        %   axis([0 sizex 0 sizey -1.5 1.5]); 
+
+        % Task 1 -> Extend to Complete Domain of sizex x sizey cells
+        axis([0 sizex 0 sizey -1.5 1.5]); 
+        xlabel('y'); ylabel('x');
+        
+        pause(0.1);
+    end
 
 end
